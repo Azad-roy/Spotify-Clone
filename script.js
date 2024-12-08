019fdeb69b31a73c511d34e3e4ef1572c785bc60
Azad Kumar <azadr1233@gmail.com>
2024-12-08 19:25:50 +0530

    Adding some songs in Your library
    Today i am adding some songs in your library section and make look's good and add a scroll bar in Your library,

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs(){
     for(let index=0; index<as.length; index++){
         const element=as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href);
+            songs.push(element.href.split("/Songs/")[1]);
         }
     }
     return songs;
@@ -21,11 +21,28 @@ async function main(){
     // Get the list of all songs.
 
     let songs= await getSongs();
-    // console.log(songs);
+    console.log(songs);
+
+    let songUl=document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUl.innerHTML=songUl.innerHTML+ `<li> <img class="invert" src="/img/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Azad</div>
+                            </div>
+                            <div class="playNow">
+                                <span>Play Now</span>
+                                <img class="invert" src="/img/play.svg" alt="">
+                            </div>
+        
+        
+        </li>`
+    }
+
 
     // Play the first song
     var audio= new Audio(songs[0]);
-    audio.play(); 
+    // audio.play(); 
     console.log("Playing: ",audio) 
 }
 main();
