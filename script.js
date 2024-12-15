8646272086c8c85503a1b0bf40ab28e57286a6ae
Azad Kumar <azadr1233@gmail.com>
2024-12-15 20:51:47 +0530

    Make buttons functional
    Today i make play, pause button functional and a seekbar functional and this seekbar is fully functional because this is sync with song, and where you move this seekbar staring, middle, end this is also change duration according to where is the positio n of the seekbar

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+let currentSong= new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs(){
+
     let a = await fetch("http://127.0.0.1:5500/Songs/");
     let response= await a.text();
     let div= document.createElement("div");
@@ -17,12 +34,28 @@ async function getSongs(){
     return songs;
 }
 
+const playMusic= (track,pause=false) =>{
+    // let audio= new Audio(/Songs/ + track);
+    currentSong.src=(/Songs/ + track)
+    if(!pause){
+        currentSong.play();
+        play.src="/img/pause.svg";
+    }
+    // console.log(currentSong.src);
+
+
+    document.querySelector(".songInfo").innerHTML= decodeURI(track);
+    document.querySelector(".songTime").innerHTML= "00:00 / 00:00";
+}
+
 async function main(){
     // Get the list of all songs.
-
     let songs= await getSongs();
-    console.log(songs);
+    
+    playMusic(songs[0], true);
+    // console.log(songs);
 
+    // Show all the songs in the playlist
     let songUl=document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
         songUl.innerHTML=songUl.innerHTML+ `<li> <img class="invert" src="/img/music.svg" alt="">
@@ -33,16 +66,45 @@ async function main(){
                             <div class="playNow">
                                 <span>Play Now</span>
                                 <img class="invert" src="/img/play.svg" alt="">
-                            </div>
-        
-        
-        </li>`
+                            </div> </li>`
     }
 
+    // Attach an event listener in each song;
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector("div").firstElementChild.innerHTML);
+            playMusic(e.querySelector("div").firstElementChild.innerHTML.trim());
+        })
+    })
 
-    // Play the first song
-    var audio= new Audio(songs[0]);
-    // audio.play(); 
-    console.log("Playing: ",audio) 
-}
+    // Attach an event listener to play, next and previous
+    play.addEventListener("click", ()=> {
+        if(currentSong.paused){
+            currentSong.play();
+            play.src="/img/pause.svg";
+        }else{
+            currentSong.pause();
+            play.src="/img/play.svg";
+        } 
+    })
+
+    // Listen for time update event
+    currentSong.addEventListener("timeupdate", ()=>{
+        // console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songTime").innerHTML=`${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+
+        document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration)*100+"%";
+    })
+
+    // Add an event listener to seekbar
+    document.querySelector(".seekBar").addEventListener("click", e=>{
+        let percent=(e.offsetX/e.target.getBoundingClientRect().width )*100;
+
+        document.querySelector(".circle").style.left=percent + "%"; 
+
+        currentSong.currentTime= (currentSong.duration*percent)/100;
+    })
+
+} 
 main();
+
